b29bd76fd4969a3dc6450bb0c22b24816301ae8b
Herbert <09tunafish1285@gmail.com>
2022-07-11 20:46:04 +0100

    Update CS Solar System.py

--- CS Solar System.py
+++ CS Solar System.py
@@ -1,3 +1,4 @@
+from symtable import Symbol
 from tkinter import *
 import pyglet
 import math
@@ -5,8 +6,12 @@ import random
 window = pyglet.window.Window(1200, 600)
 
 key = pyglet.window.key
-
+objects = []
 root = Tk()
+root.title("CS Solar System")
+screen_resolution = str(200)+'x'+str(190)
+
+root.geometry(screen_resolution)
 
 nameLabel = Label(root, text="Object Name:")
 nameLabel.grid(row=0, column=0)
@@ -48,11 +53,12 @@ var = IntVar()
 nameCheckbox = Checkbutton(root, text="Ranomize", variable=var)
 nameCheckbox.grid(row=6, column=1)
 
-currentPlanetLabel = Label(root, text="Current Planet:")
+currentPlanetLabel = Label(root, text="Current Planets:")
 currentPlanetLabel.grid(row=7, column=0)
 currentPlanets = ""
 currentPlanetslabel = Label(root, text = currentPlanets)
 currentPlanetslabel.grid(row=8, column=0)
+
 
 
 class Planet():
@@ -80,16 +86,15 @@ class Planet():
         self.circle.x = self.x
         self.circle.y = self.y
         
-objects = []
 randomnames = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"]
 #new planet
 def new_planet():
     if var.get() == 1:
-        planet = Planet(random.choice(randomnames), random.randint(200, 1000), random.randint(100, 500), random.randint(50, 500), random.randint(0, 360), random.randint(1, 25))
+        planet = Planet(random.choice(randomnames), random.randint(200, 1000), random.randint(100, 500), random.randint(50, 500), random.randint(0, 360), random.randint(1, 15))
     else:
         planet = Planet(nameEntry.get(), float(xcoordEntry.get()), float(ycoordEntry.get()), float(massEntry.get()), float(directionEntry.get()), float(velocityEntry.get()))
     objects.append(planet)
-    
+    planet.draw()
 
 generateButton = Button(root, text="Generate", command=new_planet)
 generateButton.grid(row=6, column=0)
@@ -97,46 +102,60 @@ generateButton.grid(row=6, column=0)
 running = True
 paused = False
 while running:
-    window.switch_to()
-    window.dispatch_events()
-    window.flip()
-    root.update()
-    window.clear()
-    temp_object_list = []
-    for planet in objects:
-        if planet.x > 1200 or planet.x < 0 or planet.y > 600 or planet.y < 0:
-            temp_object_list.append(planet)
+    if not paused:
+        window.switch_to()
+        window.dispatch_events()
+        window.flip()
+        root.update()
+        window.clear()
+        temp_object_list = []
+        for planet in objects:
+            if planet.x > 1200 or planet.x < 0 or planet.y > 600 or planet.y < 0:
+                temp_object_list.append(planet)
 
 
-        else:
-            planet.update()
-            planet.draw()
+            else:
+                planet.update()
+                planet.draw()
 
-    #add planets to current planets label
-    currentPlanets = ""
-    for planet in objects:
-        currentPlanets += planet.name + "\n"
-    currentPlanetslabel.config(text = currentPlanets)
-                
-    for planet in temp_object_list:
-        objects.remove(planet)
+        #add planets to current planets label
+        currentPlanets = ""
+        for planet in objects:
+            currentPlanets += planet.name + "\n"
+        currentPlanetslabel.config(text = currentPlanets)
+                    
+        for planet in temp_object_list:
+            objects.remove(planet)
+    else:
+        window.switch_to()
+        window.dispatch_events()
+        window.flip()
+        root.update()
+        currentPlanets = ""
+        for planet in objects:
+            currentPlanets += planet.name + "\n"
+        currentPlanetslabel.config(text = currentPlanets)
 
-    
+    screen_resolution = str(200)+'x'+str(190 + 15*len(objects))
+
+    root.geometry(screen_resolution)
 
 
     #detect if escape key is pressed
     @window.event
     def on_key_press(symbol, modifiers):  
         global running    
+        global paused
         if symbol == pyglet.window.key.ESCAPE:
             running = False
-
-        if symbol == pyglet.window.key.SPACE:
-            print("Paused")
-            #wait until space is pressed again
-            while symbol == pyglet.window.key.SPACE: 
-                pass                  
-
+                    
+        if paused == False and symbol == pyglet.window.key.P:
+            paused = True
+            #say paused on screen
+            pausedLabel = pyglet.text.Label("Paused", font_name='Times New Roman', font_size=16, x = 30, y=590, anchor_x='center', anchor_y='center', color=(255,255,255, 255))
+            pausedLabel.draw()
     
+        elif paused == True and symbol == pyglet.window.key.P:
+            paused = False
 
-    
+        print(paused)
