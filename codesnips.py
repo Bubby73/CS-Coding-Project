d54c192068be7d25c14a8e4937ad20d85eab2a41
Herbert <09tunafish1285@gmail.com>
2023-03-07 09:17:18 +0000

    :)

--- codesnips.py
+++ codesnips.py
@@ -1,24 +1,36 @@
-import pyglet # import pyglet library
-key = pyglet.window.key
+import pygame
+import math
 
-window = pyglet.window.Window(1200, 600) # create the window
-window.set_caption("View Window") # set the window caption
+# Set up the display
+pygame.init()
+width, height = 640, 480
+screen = pygame.display.set_mode((width, height))
 
-running = True 
+# Set up the colors
+black = (0, 0, 0)
+white = (255, 255, 255)
 
-while running: # main loop  
-    window.switch_to() 
-    window.dispatch_events()  
-    window.flip() 
-    window.clear() # clear the window at the end of each frame
+# Set up the simulation variables
+G = 6.67430e-11 # Gravitational constant
+AU = (149.6e6 * 1000) # Astronomical unit in meters
 
-    @window.event()
-    def on_key_press(symbol, modifiers):
-        global running
-        print("Key pressed")
+scale = 250 / AU
+
+class Body:
+    def __init__(self, name, mass, x_pos, y_pos):
+        self.name = name
+        self.mass = mass
+        self.x_pos = x_pos * scale + width/2 # Scale and center on screen
+        self.y_pos = y_pos * scale + height/2 # Scale and center on screen
         
-        if symbol == key.ESCAPE:
-            print("Escape key pressed")
-            running = False
-            window.close()
-         
+    def attraction(self, other):
+        dx = (self.x_pos - other.x_pos)
+        dy = (self.y_pos - other.y_pos)
+        d_squared = dx**2 + dy**2
+        
+        force_magnitude = G * self.mass * other.mass / d_squared
+        
+        theta = math.atan2(dy,dx)
+        
+        force_x_component = math.cos(theta) * force_magnitude
+        force_y_component
